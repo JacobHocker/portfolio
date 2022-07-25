c673bb8e2d033b2eebdd656eac5781745a0e85f2
Jacob Hocker <jacobdouglas06@gmail.com>
2022-07-25 08:42:07 -0500

    completed and made responsive portfolio section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = ({onDarkClick, isDark}) => {
             <a href='#' onClick={() => setActiveNav('#')} className={isDark === true ? activeNav === '#' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineHome /></a>
             <a href='#about' onClick={() => setActiveNav('#about')} className={isDark === true ? activeNav === '#about' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#about' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineUser /></a>
             <a href='#experience' onClick={() => setActiveNav('#experience')} className={isDark === true ? activeNav === '#experience' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#experience' ? 'lt-nav-link active' : 'lt-nav-link'}><BiBook /></a>
-            <a href='#porfolio' onClick={() => setActiveNav('#portfolio')} className={isDark === true ? activeNav === '#portfolio' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#portfolio' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineProject /></a>
+            <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={isDark === true ? activeNav === '#portfolio' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#portfolio' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineProject /></a>
             <a href='#contact' onClick={() => setActiveNav('#contact')} className={isDark === true ? activeNav === '#contact' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#contact' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineMessage /></a>
         </nav>
     )
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import './Portfolio.css';
+import EC from '../../assets/ec-portfolio-img.png';
+import OWSLAB from '../../assets/oslab-portfolio-img.png';
 
-const Portfolio = () => {
+const Portfolio = ({isDark}) => {
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio' className={isDark === true ? 'dk-section' : 'lt-section'}>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                <article className={isDark === true ? 'dk-portfolio__item' : 'lt-portfolio__item'}>
+                    <div className="portfolio__item-image">
+                        <img src={EC} alt='entertainment-center-image' />
+                    </div>
+                    <h3>Entertainment Center</h3>
+                    <div className="portfolio__item-cta">
+                        <a href='https://github.com' rel='noreferrer' target='_blank' className={isDark === true ? 'dk-btn' : 'lt-btn'}>
+                            GitHub
+                        </a>
+                        <a href='https://github.com' rel='noreferrer' target='_blank' className={isDark === true ? 'dk-btn dk-btn-primary' : 'lt-btn lt-primary-btn'}>
+                            Live Demo
+                        </a>
+                    </div>
+                </article>
+                <article className={isDark === true ? 'dk-portfolio__item' : 'lt-portfolio__item'}>
+                    <div className="portfolio__item-image">
+                        <img src={OWSLAB} alt='entertainment-center-image' />
+                    </div>
+                    <h3>OverStat Lab</h3>
+                    <div className="portfolio__item-cta">
+                        <a href='https://github.com' rel='noreferrer' target='_blank' className={isDark === true ? 'dk-btn' : 'lt-btn'}>
+                            GitHub
+                        </a>
+                        <a href='https://github.com' rel='noreferrer' target='_blank' className={isDark === true ? 'dk-btn dk-btn-primary' : 'lt-btn lt-primary-btn'}>
+                            Live Demo
+                        </a>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
