aabdf3771ae9ec476a3864bb417caafd5dd2fd32
Jacob Hocker <jacobdouglas06@gmail.com>
2022-07-25 15:54:08 -0500

    completed and made responsive footer component. Added favicon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {BsLinkedin, BsGithub, BsMedium} from 'react-icons/bs';
 import './Footer.css';
 
-const Footer = () => {
+const Footer = ({isDark}) => {
     return (
-        <div>Footer</div>
+        <footer className={isDark === true ? 'dk-footer' : 'lt-footer'}>
+            <a href='#' className={isDark === true ? 'dk-footer__logo' : 'lt-footer__logo'}>Jacob Hocker</a>
+
+            <ul className={isDark === true ? 'dk-permalinks' : 'lt-permalinks'}>
+                <li><a href='#' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Home</a></li>
+                <li><a href='#about' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>About</a></li>
+                <li><a href='#experience' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Experience</a></li>
+                <li><a href='#services' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Services</a></li>
+                <li><a href='#portfolio' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Portfolio</a></li>
+                <li><a href='#skills' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Skills</a></li>
+                <li><a href='#contact' className={isDark === true ? 'dk-foot-link' : 'lt-foot-link'}>Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href='https://github.com' className={isDark === true ? 'dk-foot-social' : 'lt-foot-social'}><BsGithub /></a>
+                <a href='https://medium.com' className={isDark === true ? 'dk-foot-social' : 'lt-foot-social'}><BsMedium /></a>
+                <a href='https://linkedin.com' className={isDark === true ? 'dk-foot-social' : 'lt-foot-social'}><BsLinkedin /></a>
+            </div>
+
+            <div cclassName={isDark === true ? 'dk-copyright' : 'lt-copyright'}>
+                <small>&copy; Jacob Hocker. All rights reserved.</small>
+            </div>
+        </footer>
     )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import {AiOutlineHome, AiOutlineUser, AiOutlineProject, AiOutlineMessage} from '
 import {BiBook} from 'react-icons/bi';
 import {BsLightbulb} from 'react-icons/bs';
 import {MdOutlineDarkMode} from 'react-icons/md';
+import {TbHeartHandshake} from 'react-icons/tb';
 
 import './Nav.css';
 
@@ -15,6 +16,7 @@ const Nav = ({onDarkClick, isDark}) => {
             <a href='#' onClick={() => setActiveNav('#')} className={isDark === true ? activeNav === '#' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineHome /></a>
             <a href='#about' onClick={() => setActiveNav('#about')} className={isDark === true ? activeNav === '#about' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#about' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineUser /></a>
             <a href='#experience' onClick={() => setActiveNav('#experience')} className={isDark === true ? activeNav === '#experience' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#experience' ? 'lt-nav-link active' : 'lt-nav-link'}><BiBook /></a>
+            <a href='#services' onClick={() => setActiveNav('#services')} className={isDark === true ? activeNav === '#services' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#services' ? 'lt-nav-link active' : 'lt-nav-link'}><TbHeartHandshake /></a>
             <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={isDark === true ? activeNav === '#portfolio' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#portfolio' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineProject /></a>
             <a href='#contact' onClick={() => setActiveNav('#contact')} className={isDark === true ? activeNav === '#contact' ? 'dk-nav-link active' : 'dk-nav-link' : activeNav === '#contact' ? 'lt-nav-link active' : 'lt-nav-link'}><AiOutlineMessage /></a>
         </nav>
