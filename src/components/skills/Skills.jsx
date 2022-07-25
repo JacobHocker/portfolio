d02088fc8e3f6cac84fe400009b6bee144d57397
Jacob Hocker <jacobdouglas06@gmail.com>
2022-07-25 10:43:55 -0500

    completed and made responsive skills section. installed swiper.js and added language pictures

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import REACT from '../../assets/lang-logo/react-transparent.png';
+import HTML from '../../assets/lang-logo/html-transparent.png';
+import CSS from '../../assets/lang-logo/css-transparent.png';
 import './Skills.css';
 
-const Skills = () => {
+// import Swiper core and required modules
+import {  Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+
+
+const skillData = [
+    {
+        avatar: REACT,
+        name: 'React.js',
+        content: 'This is where the content for the skill will go.  Testing how long text should be'
+    }, 
+    {
+        avatar: HTML,
+        name: 'HTML5',
+        content: 'This is where the content for the skill will go.  Testing how long text should be'
+    }, 
+    {
+        avatar: CSS,
+        name: 'CSS3',
+        content: 'This is where the content for the skill will go.  Testing how long text should be'
+    }
+]
+
+const Skills = ({isDark}) => {
     return (
-        <section id='skills'>Skills</section>
+        <section id='skills' className={isDark === true ? 'dk-section' : 'lt-section'}>
+            <h5>The Knowledge and Abilities I Can Bring</h5>
+            <h2>Skills</h2>
+
+            <Swiper className="container skills__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            
+            >
+                {skillData.map(({avatar, name, content}, index) => {
+                    return (
+                        <SwiperSlide key={index} className={isDark === true ? 'dk-skill' : 'lt-skill'}>
+                            <div className="skill__avatar">
+                                <img src={avatar} alt='react-image' className='avatar'/>
+                                <div className={isDark === true ? 'dk-skill__overlay' : 'lt-skill__overlay'} >
+                                    <h2 className={isDark === true ? 'dk-skill__name' : 'lt-skill__name'}>{name}</h2>
+                                    <small className={isDark === true ? 'dk-skill__content' : 'lt-skill__content'}>
+                                        {content}
+                                    </small>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </section>
     )
 }
 
